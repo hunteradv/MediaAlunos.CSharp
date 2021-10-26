b18159a1cdc4b34227af219aa73a78d5334f8594
hunteradv <gustavopaulody@gmail.com>
2021-10-25 21:52:55 -0300

    adding struct Aluno and enum Conceito | insert, list and calculate average

--- Program.cs
+++ Program.cs
@@ -6,6 +6,93 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            int indiceAluno = 0;
+            string opcaoUsuario = GetOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Digite a nota do aluno: ");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor da nota deve ser decimal!");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if(!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                            }
+                        }
+
+                        break;
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+                        for (int i=0; i < alunos.Length; i++)
+                        {
+                            if(!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Conceito conceitoGeral;
+
+                        if (mediaGeral <= 2)
+                        {
+                            conceitoGeral = Conceito.E;
+                        }
+                        else if (mediaGeral <= 4)
+                        {
+                            conceitoGeral = Conceito.D;
+                        }
+                        else if (mediaGeral <= 6)
+                        {
+                            conceitoGeral = Conceito.C;
+                        }
+                        else if (mediaGeral <= 8)
+                        {
+                            conceitoGeral = Conceito.B;
+                        }
+                        else
+                        {
+                            conceitoGeral = Conceito.A;
+                        }
+
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
+                        
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = GetOpcaoUsuario();
+            }
+        }
+
+        private static string GetOpcaoUsuario()
+        {
+            Console.WriteLine();
             Console.WriteLine("Informe a opção desejada");
             Console.WriteLine("1- Inserir novo aluno");
             Console.WriteLine("2- Listar alunos");
@@ -14,36 +101,7 @@ namespace Revisao
             Console.WriteLine();
 
             string opcaoUsuario = Console.ReadLine();
-
-            while ( opcaoUsuario.ToUpper() != "X")
-            {
-                switch(opcaoUsuario)
-                {
-                    case "1":
-                        //TODO: adicionar aluno
-
-                        break;
-                    case "2":
-                        //TODO listar alunos
-
-                        break;
-                    case "3":
-                        //TODO calcular media geral
-
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                Console.WriteLine("Informe a opção desejada");
-                Console.WriteLine("1- Inserir novo aluno");
-                Console.WriteLine("2- Listar alunos");
-                Console.WriteLine("3- Calcular média diária");
-                Console.WriteLine("X- Sair");
-                Console.WriteLine();
-
-                opcaoUsuario = Console.ReadLine();
-            }
+            return opcaoUsuario;
         }
     }
 }
